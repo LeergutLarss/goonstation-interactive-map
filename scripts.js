254bcf29bb27f703d87b805baf2c1cfd3fe99073
LeergutLars <119765991+LeergutLarss@users.noreply.github.com>
2023-03-27 21:20:58 +0200

    Fixed map image resolution

--- scripts.js
+++ scripts.js
@@ -1,11 +1,9 @@
 const imageBounds = [[0, 0], [9600, 9600]];
-const fieldOfViewBound = [[800, 0], [7800, 9600]];
 
 var map = L.map('map', {
     crs: L.CRS.Simple,
     minZoom: -3,
-    
-    maxBounds: fieldOfViewBound
+    maxZoom: 1
 });
 
 var mapLayer = L.imageOverlay('./atlas-1.png', imageBounds).addTo(map);
