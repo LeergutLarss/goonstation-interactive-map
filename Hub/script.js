b2fad6b2c23ca12710feb09556b07e60cc452867
LeergutLars <119765991+LeergutLarss@users.noreply.github.com>
2023-03-29 11:34:35 +0200

    Create mapNodes Script

--- Hub/script.js
+++ Hub/script.js
@@ -1,9 +1,17 @@
 
-const mapNode = document.createElement('div');
-const mapImage = document.createElement('img');
+const images = ['../Atlas/images/atlas-1.png', '../Cogmap1/images/cogmap-1.png', '../Cogmap2/images/cogmap2-1.png'];
 
+function createNodes(imageSource) {
 
-mapNode.classList.add('mapNode');
+    imageSource.forEach(source => {
+        const mapNode = document.createElement('div');
+        const mapImage = document.createElement('img');
+    
+        mapNode.classList.add('mapNode');
+        mapImage.src = source;
+        mapNode.appendChild(mapImage);
+        document.getElementById('main').appendChild(mapNode);
+    });
+}
 
-mapNode.appendChild(mapImage);
-document.getElementById('main').appendChild(mapNode);
+createNodes(images);
