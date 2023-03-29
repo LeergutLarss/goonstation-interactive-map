105c3866b9eb2da307d370f2827a737d22330c39
LeergutLars <119765991+LeergutLarss@users.noreply.github.com>
2023-03-29 11:17:25 +0200

    CSS for imageNode

---
+++ Hub/script.js
@@ -0,0 +1,9 @@
+
+const mapNode = document.createElement('div');
+const mapImage = document.createElement('img');
+
+
+mapNode.classList.add('mapNode');
+
+mapNode.appendChild(mapImage);
+document.getElementById('main').appendChild(mapNode);
