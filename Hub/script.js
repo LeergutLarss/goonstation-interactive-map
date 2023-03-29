da628eb929745cae727d130df913da9873bbd89c
LeergutLars <119765991+LeergutLarss@users.noreply.github.com>
2023-03-29 11:57:39 +0200

    MapNodes now links to maps

--- Hub/script.js
+++ Hub/script.js
@@ -4,18 +4,18 @@ const images = ['../Atlas/images/atlas-1.png', '../Cogmap1/images/cogmap-1.png',
 function createNodes(imageSource) {
 
     imageSource.forEach(source => {
-        const mapContainer = document.createElement('div');
+        const mapContainer = document.createElement('a');
         const mapImage = document.createElement('img');
         const mapName = document.createElement('p2');
 
         mapContainer.classList.add('mapNode');
-
+        mapContainer.href = source.slice(0, source.indexOf('/i')) + '/index.html';
         mapImage.src = source;
         mapContainer.appendChild(mapImage);
 
         mapName.textContent = source.slice(3, source.indexOf('/i')); 
         mapContainer.appendChild(mapName);
-        
+
         document.getElementById('main').appendChild(mapContainer);
     });
 }
