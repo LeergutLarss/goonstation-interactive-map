5775b1a078b561b3e4b1a1764b13eb27fb11b07a
LeergutLars <119765991+LeergutLarss@users.noreply.github.com>
2023-03-29 11:47:52 +0200

    Add image title

--- Hub/script.js
+++ Hub/script.js
@@ -4,13 +4,19 @@ const images = ['../Atlas/images/atlas-1.png', '../Cogmap1/images/cogmap-1.png',
 function createNodes(imageSource) {
 
     imageSource.forEach(source => {
-        const mapNode = document.createElement('div');
+        const mapContainer = document.createElement('div');
         const mapImage = document.createElement('img');
-    
-        mapNode.classList.add('mapNode');
+        const mapName = document.createElement('p2');
+
+        mapContainer.classList.add('mapNode');
+
         mapImage.src = source;
-        mapNode.appendChild(mapImage);
-        document.getElementById('main').appendChild(mapNode);
+        mapContainer.appendChild(mapImage);
+
+        mapName.textContent = source.slice(3, source.indexOf('/i')); 
+        mapContainer.appendChild(mapName);
+        
+        document.getElementById('main').appendChild(mapContainer);
     });
 }
 
