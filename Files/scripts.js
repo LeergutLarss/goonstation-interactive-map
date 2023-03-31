a03bd235264cc7d83b5e6798b215257367853ede
LeergutLars <119765991+LeergutLarss@users.noreply.github.com>
2023-03-31 20:44:57 +0200

    Add pipnet to maps

--- Files/scripts.js
+++ Files/scripts.js
@@ -1,12 +1,25 @@
+var mapLayer = L.imageOverlay(window.imageSource, window.imageBounds);
+var pipeLayer = L.imageOverlay(window.pipeSource, window.imageBounds);
 
 var map = L.map('map', {
     crs: L.CRS.Simple,
     minZoom: -3,
     maxZoom: 1,
-    maxBounds: window.imageBounds
+    maxBounds: window.imageBounds,
+    layers: mapLayer
 });
 
-var mapLayer = L.imageOverlay(window.imageSource, window.imageBounds).addTo(map);
+
+var baseLayers = {
+    "Base": mapLayer,
+};
+
+var overlays = {
+    "PipeNet": pipeLayer,
+};
+
+var layerControl = L.control.layers(baseLayers, overlays).addTo(map);
+
 
 map.fitBounds(window.imageBounds);
 
